5373079bc095ada76d3b0bc5e131a6d87e910ce1
sithortodox <zteserv@gmail.com>
2026-04-16 19:29:20 +0300

    Fix DB path: mount volume as directory

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ GITHUB_TOKEN = os.getenv("GITHUB_TOKEN", "")
 CHAT_ID = os.getenv("CHAT_ID", "")
 CHECK_INTERVAL_MINUTES = int(os.getenv("CHECK_INTERVAL_MINUTES", "60"))
 CHECK_TIMEOUT_SECONDS = int(os.getenv("CHECK_TIMEOUT_SECONDS", "5"))
-DB_PATH = os.getenv("DB_PATH", "v2ray_bot.db")
+DB_PATH = os.getenv("DB_PATH", "/app/data/v2ray_bot.db")
 
 GITHUB_API_URL = "https://api.github.com"
 GITHUB_RAW_URL = "https://raw.githubusercontent.com"
